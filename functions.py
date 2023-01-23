543f6233e8049d7f8d260455f009539f99a7f39c
Pasi P <pjpakkala@gmail.com>
2023-01-23 19:06:57 +0000

    Initial commit

---
+++ functions.py
@@ -0,0 +1,1 @@
+#used for all the functions
---
+++ main.py
@@ -0,0 +1,11 @@
+#setting up files and tasks
+
+#TASKS
+# 1 Scrape the Replit Community Hub for events and put them into a list.
+    #Using BeautifulSoup
+# 2 Filter the events by topics that interest you (remember how we filtered news articles from hacker news?)
+# 3 Schedule the scrape for every 6 hours.
+    #Using Scheduling
+# 4 If an event of interest is scraped, email yourself (or a friend) with a hyperlink to the event.
+    #Using Gmail
+# 5 Only email any new events.
