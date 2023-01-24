106142e8d8aebb27be76be1f056fc8e898d9f7ad
Pasi P <pjpakkala@gmail.com>
2023-01-24 20:24:38 +0000

    Scheduling has been built.
    Only need to save the results in database now and combine all functions to work together.

--- functions.py
+++ functions.py
@@ -1,8 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
+import schedule, time, os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEtext
 
-URL = "https://replit.com/community-hub"
 
-response = requests.get(URL)
-html = response.text
-print(html)
+
+def web_crawler():
+    URL = "https://replit.com/community-hub"
+    
+    response = requests.get(URL)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    
+    #Cards picks up the part of the page code where wanted titles and dates are
+    cards = soup.find("div", {"class":"css-nghpcj"})
+    titles = cards.find_all("span", {"class":"css-19l40in"})
+    dates = cards.find_all("span", {"class":"css-1jm4vlb"})
+    
+    counter = 0
+    for title in titles:
+        print(title.text)
+        for date in dates[counter]:
+            print(date.text)
+            counter += 1
+        print()
+
+
+def email_sending():
+    
+    password = os.environ['app_password']
+    username = os.environ['email']
+    
+    email = pass # to be added - any new addition web crawler finds
+    server = "smtp.gmail.com"
+    port = 587
+    s = smtplib.SMTP(host=server, port=port)
+    s.starttls()
+    s.login(username, password)
+
+    message = MIMEMultipart()
+    message["to"] = username
+    message["from"] = "Monco C."
+    message["subject"] = "New event"
+    message.attach(MIMEText(email, "html"))
+
+    s.send_message(message)
+    del message
+
+
+def combined_function()
+    pass
--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 #setting up files and tasks
 
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://replit.com/community-hub"
-
-response = requests.get(URL)
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
-
-cards = soup.find("div", {"class":"css-nghpcj"})
-
-
-titles = cards.find_all("span", {"class":"css-19l40in"})
-dates = cards.find_all("span", {"class":"css-1jm4vlb"})
-
-counter = 0
-for title in titles:
-    print(title.text)
-    for date in dates[counter]:
-        print(date.text)
-        counter += 1
-    print()
     
+import schedule, time
 
-
+schedule.every(5).minutes.do(combined_function)
 
 #TASKS
-# 1 Scrape the Replit Community Hub for events and put them into a list.
-    #Using BeautifulSoup
+# 1 Scrape the Replit Community Hub for events and put them into a list. ok
+    #Using BeautifulSoup ok
     #save to database
-# 2 Filter the events by topics that interest you (remember how we filtered news articles from hacker news?)
-# 3 Schedule the scrape for every 6 hours.
-    #Using Scheduling
+# 2 Filter the events by topics that interest you (remember how we filtered news articles from hacker news?)  pass
+# 3 Schedule the scrape for every 6 hours. ok (5min for testing)
+    #Using Scheduling ok
 # 4 If an event of interest is scraped, email yourself (or a friend) with a hyperlink to the event.
-    #Using Gmail
+    #Using Gmail ok
+    #save password etc in Secrets ok
 # 5 Only email any new events.
