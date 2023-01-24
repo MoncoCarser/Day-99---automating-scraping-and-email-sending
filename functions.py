a5ead8ed34c9bd182b40eceefc44f174ac26a54f
Pasi P <pjpakkala@gmail.com>
2023-01-24 18:58:05 +0000

    Web crawler code now works.

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
-#used for all the functions
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://replit.com/community-hub"
+
+response = requests.get(URL)
+html = response.text
+print(html)
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 #setting up files and tasks
 
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://replit.com/community-hub"
+
+response = requests.get(URL)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+
+cards = soup.find("div", {"class":"css-nghpcj"})
+
+
+titles = cards.find_all("span", {"class":"css-19l40in"})
+dates = cards.find_all("span", {"class":"css-1jm4vlb"})
+
+counter = 0
+for title in titles:
+    print(title.text)
+    for date in dates[counter]:
+        print(date.text)
+        counter += 1
+    print()
+    
+
+
+
 #TASKS
 # 1 Scrape the Replit Community Hub for events and put them into a list.
     #Using BeautifulSoup
+    #save to database
 # 2 Filter the events by topics that interest you (remember how we filtered news articles from hacker news?)
 # 3 Schedule the scrape for every 6 hours.
     #Using Scheduling
